ba7738e96e12d1f0492c30a22581cc3dec759169
Michael Davidson <mrmdavidson@sadorangeproductions.com>
2015-08-31 00:53:12 +1000

    Tweaked generate script

--- generate.bat
+++ generate.bat
@@ -1,5 +1,8 @@
+pushd ..\mrmdavidson.github.io.output
+git pull origin
+popd
 Tools\pretzel bake --destination ..\mrmdavidson.github.io.output\
 pushd ..\mrmdavidson.github.io.output
 git commit -am "Generated site"
-git push
+git push origin master:master
 popd
